248937095870e4ee109094697d33240a1eab977a
April Houk <april.t.houk@gmail.com>
2024-11-03 04:42:22 -0500

    ss

---
+++ Spotlight.Models/Music/NoteMode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spotlight.Models
+{
+    public enum NoteMode
+    {
+        Whole = 0xC,
+        Half = 0xA,
+        DottedHalf = 0x8,
+        DottedQuarter = 0xB,
+        DottedEight = 0x5,
+        Quarter = 0x8,
+        Eigth = 0x4,
+        Sixteenth = 0x1,
+        QuarterTriplet = 0x6,
+        EightTriplet = 0x2
+    }
+}
